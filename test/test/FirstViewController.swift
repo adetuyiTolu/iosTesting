0e976550ae68ecad2501d03fe51ba1b5f11a6e02
Adetuyi Tolu Emmanuel <tolu.adetuyi@gmail.com>
2017-08-13 15:12:20 +0100

    update UI controller

---
+++ test/test/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  test
+//
+//  Created by Adetuyi Tolu Emmanuel on 8/13/17.
+//  Copyright © 2017 Adetuyi Tolu Emmanuel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
